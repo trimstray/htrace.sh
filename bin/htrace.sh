5e00be98f18f4cb4848db3cddf52376edb486ed2
trimstray <trimstray@gmail.com>
2018-07-14 09:01:48 +0200

    updated htrace.sh version - signed-off-by: trimstray <trimstray@gmail.com>

--- bin/htrace.sh
+++ bin/htrace.sh
@@ -58,7 +58,7 @@ source "${_src}/helpers"
 # shellcheck disable=SC1090
 source "${_src}/__init__"
 
-readonly _version="1.0.0"
+readonly _version="1.0.1"
 
 
 # We pass arguments to the __main__ function.
