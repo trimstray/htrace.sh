49697b483158e355a7909009db227e821f8b9db5
@trimstray <trimstray@gmail.com>
2019-03-05 15:50:14 +0100

    disable shellcheck warnings

--- bin/htrace.sh
+++ bin/htrace.sh
@@ -44,6 +44,7 @@ if [[ "$(uname)" == "Darwin" ]] ; then
   [ ! -z "$(brew --prefix)" ] && PATH=$(brew --prefix)/opt/gnu-getopt/bin:$(brew --prefix)/opt/gnu-sed/libexec/gnubin:$(brew --prefix)/opt/coreutils/libexec/gnubin:$PATH
   [ ! -z "$(composer global config bin-dir --absolute 2>/dev/null)" ] && PATH=$(composer global config bin-dir --absolute 2>/dev/null):$PATH
 
+# shellcheck disable=SC2003,SC2046
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]] ; then
   true
 else
